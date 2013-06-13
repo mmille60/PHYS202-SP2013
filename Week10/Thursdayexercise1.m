6afbcf2069f2b152b7feb959ad85430d15bd975f
Kyle Miller <mmille60@calpoly.edu>
2013-06-12 23:41:46 -0700

    updating

--- Week10/Thursdayexercise1.m
+++ Week10/Thursdayexercise1.m
@@ -24,6 +24,7 @@ figure(5)
 plot(x,x.^2)
 %%
 % function to fit 
+figure(6)
 fun = @(a,b,c,x) -sqrt(a^2-(x-b).^2)+c;
 % Find a starting point for the parameters a, b, and c.
 guess = fun(15,0,15,x); % fun(a,b,c,x)
@@ -32,3 +33,25 @@ plot(x,guess,'r:');
 fittedmodel = fit(x',y',fun,'StartPoint',[15 0 15]);
 % plot the result
 plot(fittedmodel,'r-');
+
+% fit the data using the uncertainties as weights
+w = ey.^-2;
+weightedfitted = fit(x',y',fun,'StartPoint',[15 0 15],'Weights',w');
+% plot the result
+plot(weightedfitted,'r-');
+%%
+%exercise 2
+experiment = importdata('radioactivedecay.dat');
+t = experiment.data(:,1);
+N = experiment.data(:,2);
+fun2 = @(a,b,c,x) b*exp(-a*x)+c;
+figure(42)
+plot(t,N,'.b');
+figure(53)
+plot(t,N,'.b');
+fittedmodel = fit(t,N,fun2,'StartPoint',[1 9 0]);
+plot(t,N,'.b');
+% plot the result
+plot(fittedmodel,'r-',t,N,'.b');
+legend('radioative data','curvefit')
+xlabel('time (s)'); ylabel('number of decays');
